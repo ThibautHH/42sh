4b6a0752ac09d42a9499c6d1422bcfbfc107c91d
Raphael_m <raphael.mabille@epitech.eu>
2023-05-09 16:55:16 +0200

    Style indentation thingy

--- src/mysh.c
+++ src/mysh.c
@@ -40,8 +40,8 @@ void mysh(mysh_t *context, char **env)
         exit(84);
     prompt(context), errno = 0;
     for (; !EXIT && GET_LINE != -1; LINE_ITERATION)
-        if (LEN > 1 && *LINE != '#' &&
-        substitute_alias(context) && !parse_command_line(context))
+        if (LEN > 1 && *LINE != '#' && substitute_alias(context) 
+            && !parse_command_line(context))
             TAILQ_FOREACH(PIPELINE, &context->pipelines, entries)
                 run_pipeline(context);
     if (errno)
