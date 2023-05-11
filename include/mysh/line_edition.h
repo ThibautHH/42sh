220ae34bebfb373aa76b854786f968b4c701bf51
Blaizot <guillaume.blaizot@epitech.eu>
2023-05-11 12:31:08 +0200

    FEAT(<line_edition>): add line_edition.h

--- include/mysh/line_edition.h
+++ include/mysh/line_edition.h
@@ -6,11 +6,10 @@
 */
 
 #ifndef LINE_EDITION_H_
-#define LINE_EDITION_H_
+    #define LINE_EDITION_H_
 
     #include "mysh.h"
 
     void input_edition(mysh_t *context);
 
 #endif /* !LINE_EDITION_H_ */
-
