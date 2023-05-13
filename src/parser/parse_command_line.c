de0da2c2f7b285719ee1c530c94bedfba7ddbe34
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2023-05-13 23:27:07 +0200

    Added blackslashes removal

--- src/parser/parse_command_line.c
+++ src/parser/parse_command_line.c
@@ -63,11 +63,15 @@ static void tokenize(mysh_t *context)
         if (!CMDARGS)
             DIE;
         DUP_ARG(CMDARGS[CMDARGC - 1]);
-        char *tmp = P;
-        for (P = CMDARGS[CMDARGC - 1]; *P;)
-            if (!unquote(context))
-                P++;
-        P = tmp;
+        char *ptmp = P, *stmp = S;
+        for (P = (S = CMDARGS[CMDARGC - 1]); *P;)
+            (void)(unquote(context) || P++);
+        for (P = CMDARGS[CMDARGC - 1]; *P; P++)
+            (void)(IS_CHAR_ESCAPED && strcpy(P - 1, P));
+        for (P = CMDARGS[CMDARGC - 1]; *P; P++)
+            (void)(*P == '\\' && P[1] == '\\' && P[2] != '\\'
+                && strcpy(P, P + 1));
+        P = ptmp, S = stmp;
     }
     CMDARGS[CMDARGC] = NULL;
 }
