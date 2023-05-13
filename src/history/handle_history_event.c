f315dcfe8093fd5c28008c8d279a7df6ff647d77
DiamonsGold <romain.hure@epitech.eu>
2023-05-13 17:28:50 +0200

    fix(Hystory_v2): remove segv when use history event at start and detection of history event.

--- src/history/handle_history_event.c
+++ src/history/handle_history_event.c
@@ -5,33 +5,36 @@
 ** handle_history_event
 */
 
-#include "mysh/history.h"
-#include "mysh.h"
-
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "mysh/history.h"
 
 void realloc_line(mysh_t *context, char *line, int offset)
 {
-    size_t newSize = LEN + strlen(line) + 1;
-    char *rest_of_line = strdup(LINE + offset);
+    size_t len = LEN + strlen(line) + 1;
 
-    if (newSize > LINESZ) {
-        LINESZ = newSize;
+    if (len > LINESZ) {
+        LINESZ = len;
         LINE = realloc(LINE, LINESZ);
         if (LINE == NULL)
             DIE;
     }
-    strcpy(LINE + offset, line);
-    strcat(LINE + offset + strlen(line), rest_of_line);
-    free(rest_of_line);
+    sprintf(LINE, "%s%s", line, LINE + offset + 1);
 }
 
-_Bool handle_history_event(mysh_t *context)
+static void get_offset(mysh_t *context, int *offset)
+{
+    for ((*offset)++; !IS_WHITESPACE(LINE[*offset]); (*offset)++);
+}
+
+bool handle_history_event(mysh_t *context)
 {
     for (int i = 0; LINE[i]; i++) {
-        if (LINE[i] == '!')
+        if (LINE[i] == '!') {
+            get_offset(context, &i);
             history_event(context, i);
+        }
     }
     return true;
 }
--- src/history/history_event.c
+++ src/history/history_event.c
@@ -5,30 +5,27 @@
 ** builtin_event
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#include "mysh.h"
 #include "mysh/history.h"
-#include "ice/int.h"
 
 static int handle_precise_event(mysh_t *context, int offset)
 {
-    size_t cmd_size = ice_strlen(LINE) - 2;
-    int j = 0;
+    size_t cmd_size = strlen(LINE) - 2;
     char *str = malloc(sizeof(char) * (cmd_size + 2));
-    history_t *tmp;
+    history_t *history;
+    int j = 0;
 
-    if (LINE[1] != '?' || str == NULL) return 1;
+    if (LINE[1] != '?' || str == NULL)
+        return 1;
     for (int i = 2; LINE[i] != '\0' && LINE[i] != '?'; i++, j++)
         str[j] = LINE[i];
     str[j] = '\0';
-    for (list_node_t *node = HISTORY->tail; node != NULL;
-        node = node->prev) {
-        tmp = node->value;
-        if (ice_strstr(tmp->cmd, str) != NULL) {
-            realloc_line(context, tmp->cmd, offset);
-            printf("%s\n", LINE);
+    for (list_node_t *node = HISTORY->tail; node; node = node->prev) {
+        history = node->value;
+        if (ice_strstr(history->cmd, str) != NULL) {
+            realloc_line(context, history->cmd, offset);
             return 0;
         }
     }
@@ -38,17 +35,15 @@ static int handle_precise_event(mysh_t *context, int offset)
 
 static int handle_search_event(mysh_t *context, int offset)
 {
-    int nb_event = ice_atoi(LINE + 1);
+    int nb_event = atoi(LINE + 1);
     history_t *tmp;
 
     if (nb_event <= 0)
         return 1;
-    for (list_node_t *node = HISTORY->tail; node != NULL;
-        node = node->prev) {
+    for (list_node_t *node = HISTORY->tail; node; node = node->prev) {
         tmp = node->value;
         if (nb_event == tmp->index) {
             realloc_line(context, tmp->cmd, offset);
-            printf("%s\n", LINE);
             return 0;
         }
     }
@@ -58,15 +53,14 @@ static int handle_search_event(mysh_t *context, int offset)
 
 static int handle_last_event(mysh_t *context, int offset)
 {
-    history_t *tmp;
+    history_t *history;
     list_node_t *node = HISTORY->tail;
 
     if (node == NULL)
         return 0;
-    tmp = node->value;
+    history = node->value;
     if (LINE[1] == '!' && LINE[2] == '\0') {
-        realloc_line(context, tmp->cmd, offset);
-        printf("%s\n", LINE);
+        realloc_line(context, history->cmd, offset);
         return 0;
     }
     return 1;
@@ -85,7 +79,6 @@ static int search_in_history(char *cmd, mysh_t *context, int offset,
     ice_strncpy(str2, cmd, cmd_size);
     if (ice_strcmp(str2, str1) == 0) {
         realloc_line(context, cmd, offset);
-        printf("%s\n", LINE);
         return 1;
     }
     if (count > HISTORY->size)
@@ -95,20 +88,16 @@ static int search_in_history(char *cmd, mysh_t *context, int offset,
 
 void history_event(mysh_t *context, int offset)
 {
-    history_t *tmp;
+    history_t *history;
     ull_t count = 0;
 
-    if (handle_precise_event(context, offset) == 0)
+    if (!handle_precise_event(context, offset)
+        || !handle_search_event(context, offset)
+        || !handle_last_event(context, offset))
         return;
-    if (handle_search_event(context, offset) == 0)
-        return;
-    if (handle_last_event(context, offset) == 0)
-        return;
-    for (list_node_t *node = HISTORY->tail; node != NULL;
-    node = node->prev, count++) {
-        tmp = node->value;
-        if (search_in_history(tmp->cmd, context, offset, count) == 1)
+    for (list_node_t *node = HISTORY->tail; node; node = node->prev, count++) {
+        history = node->value;
+        if (search_in_history(history->cmd, context, offset, count) == 1)
             break;
     }
-    return;
 }
