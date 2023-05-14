33f08bdce50a4a22929fedb9bd3f7047eab74d50
DiamonsGold <romain.hure@epitech.eu>
2023-05-14 20:08:05 +0200

    feat(clean): exit handle wrong input.

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -9,12 +9,19 @@
 
 void builtin_exit(mysh_t *context)
 {
+    char *endptr;
+
     if (CMDARGC > 2) {
         DWRITE(STDERR_FILENO, "exit: Expression Syntax.\n", 25);
         STATUS = 1;
         return;
     }
     EXIT = 1;
-    if (CMDARGC == 2)
-        STATUS = atoi(CMDARGS[1]);
+    if (CMDARGC == 2) {
+        STATUS = strtol(CMDARGS[1], &endptr, 10);
+        if (*endptr) {
+            DWRITE(STDERR_FILENO, "exit: Badly formed number.\n", 27);
+            STATUS = 1;
+        }
+    }
 }
